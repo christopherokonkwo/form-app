b52858f1c70d2bfdce98d6a948a29fdfa1675d7a
Christopher Okonkwo <xtopherc43@gmail.com>
2022-07-15 22:58:38 +0100

    misc updates

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }} ― Dashboard</title>
+    <title>{{ config('app.name') }} ― Dashboard </title>
 
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
